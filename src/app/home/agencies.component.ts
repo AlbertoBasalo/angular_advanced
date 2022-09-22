10764d9bbd82002145c16ba55950dade5fced9a1
Alberto Basalo <albertobasalo@hotmail.com>
2022-09-22 11:54:33 +0200

    feat: pre to speed improve

--- src/app/home/agencies.component.ts
+++ src/app/home/agencies.component.ts
@@ -16,22 +16,19 @@ import { data } from "../data.repository";
   ],
   template: `
     <article>
-      <h3>We work with {{ agencies.length }} agencies</h3>
-      <ul *ngIf="agencies.length > 0; else noContent">
+      <h3>{{ getHeader() }}</h3>
+      <ul>
         <li *ngFor="let agency of agencies">
-          <span [ngClass]="getClassForStatus(agency.status)">
-            {{ agency.name }}
-          </span>
+          <span [ngClass]="byStatus(agency.status)">{{ agency.name }}</span>
           <span *ngIf="agency.range === 'Interplanetary'">🪐</span>
           <span *ngIf="agency.range === 'Orbital'">🌍</span>
         </li>
       </ul>
-      <ng-template #noContent>🕳️ No data yet</ng-template>
     </article>
   `,
 })
 export class AgenciesComponent {
   agencies = data.agencies;
-  getAgenciesCounter = () => this.agencies.length;
-  getClassForStatus = (status: string) => status.toLowerCase();
+  byStatus = (status: string) => status.toLowerCase();
+  getHeader = () => `We work with ${this.agencies.length} agencies`;
 }
